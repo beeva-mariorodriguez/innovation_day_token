d63e705f3a8949a9e34829658382032bf340f2a5
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-05-16 16:46:43 +0200

    WIP! deploy to private network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,10 @@
 var InnovationDay = artifacts.require("InnovationDay");
-module.exports = function(deployer) {
-    deployer.deploy(InnovationDay);
+module.exports = function(deployer, network) {
+    if (network == "development") {
+        deployer.deploy(InnovationDay, {from:"0x627306090abab3a6e1400e9345bc60c78a8bef57"});
+    }
+    if (network == "live") {
+        deployer.deploy(InnovationDay, {from:"0810ce69e1ce41c0d68e4baa09391da1c2392d49"});
+    }
 };
+
