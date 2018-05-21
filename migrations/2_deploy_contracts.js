253606d707196b415523f9d1ea6662c5870ce80d
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-05-21 09:47:46 +0200

    0.1.1 (new build files)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ module.exports = function(deployer, network) {
         deployer.deploy(InnovationDay, {from:"0x627306090abab3a6e1400e9345bc60c78a8bef57"});
     }
     if (network == "live") {
-        deployer.deploy(InnovationDay, {from:"0xe46442286fa6ce88d46024d55b736cfcc2eac02e"});
+        deployer.deploy(InnovationDay);
     }
 };
 
